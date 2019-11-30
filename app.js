60a873849e77fd63a4f30db38d0a96c8f9237c77
ItzPlayMind <mgraf300502@gmail.com>
2019-11-30 14:42:23 +0100

    Changes

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const fs = require("fs");
-const port = process.env.PORT || 3443;
+const port = process.env.PORT || 3000;
 
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require("socket.io")(http);
-
-app.get('/', function(req, res){
-    res.sendFile("index.html");
-});
 
 io.on("connection", (socket)=>{
     console.log("A user joined!");
