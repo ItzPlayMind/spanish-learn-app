af3172ed8671c608f8b1ad796b0de3fdc58f298f
ItzPlayMind <mgraf300502@gmail.com>
2019-11-30 14:23:59 +0100

    Changed Port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const fs = require("fs");
-const port = 3443;
+const port = process.env.PORT || 3443;
 
 const server = http.createServer((req,res)=>{
     res.writeHead(200, { 'Content-Type': 'text/html' });
